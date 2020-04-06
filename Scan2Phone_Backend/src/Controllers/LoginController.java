f274d0856f342cb1c08affe6c9f33544b1156208
McGarvelcuddy <mcgarvelcuddy@gmail.com>
2020-04-06 18:04:44 -0400

    Created NetBeans file structure, added jar file that we will use and tested structure.

---
+++ Scan2Phone_Backend/src/Controllers/LoginController.java
@@ -0,0 +1,25 @@
+package Controllers;
+
+import org.json.simple.JSONObject;
+
+public class LoginController {
+    JSONObject account;
+
+
+    /**
+     * 
+     * @param uName
+     * @param password
+     * 
+     * https://www.tutorialspoint.com/json/json_java_example.htm
+     * 
+     */
+    public void accountSelect(String uName, String password){
+
+    }
+
+    
+    public void beginAddCard(){
+
+    }
+}
