4519717852149e326c76621d63f1a9be580b5376
McGarvelcuddy <mcgarvelcuddy@gmail.com>
2020-04-14 13:07:32 -0400

    Created LoginView without any functionality or possible error checking

--- Scan2Phone_Backend/src/Views/LoginView.java
+++ Scan2Phone_Backend/src/Views/LoginView.java
@@ -1,5 +1,98 @@
 package Views;
 
-public class LoginView {
+import Controllers.LoginController;
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.awt.Font;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
 
+public class LoginView extends JFrame {
+    private final LoginController controller;
+    private JLabel instructions;
+    private JLabel uNameLabel;
+    private JLabel pWordLabel;
+    private JTextField username;
+    private JTextField password;
+    private JButton loginButton;
+    private JPanel entryArea;
+    private JPanel gridPanel;
+    
+    public LoginView(LoginController controller){
+        this.controller = controller;
+        initComponents();
+    }
+    
+    private void initComponents() {
+        setTitle("Scan2Phone - Login");
+        setSize(600, 600);
+        
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        
+        instructions = new JLabel("Please log in to your Scan2Phone account created on our mobile app to add cards.");
+        
+        uNameLabel = new JLabel("Username");
+        uNameLabel.setPreferredSize(new Dimension(70, 21));
+        pWordLabel = new JLabel("Password");
+        pWordLabel.setPreferredSize(new Dimension(70, 21));
+        
+        username = new JTextField();
+        password = new JTextField();
+        
+        username.setPreferredSize(new Dimension(70, 21));
+        password.setPreferredSize(new Dimension(70, 21));
+        
+        loginButton = new JButton("Login");
+        
+        entryArea = new JPanel();
+        entryArea.add(uNameLabel);
+        entryArea.add(username);
+        entryArea.add(pWordLabel);
+        entryArea.add(password);
+        
+        this.setContentPane(new JPanel(new BorderLayout()));
+        
+        gridPanel = new JPanel(new GridBagLayout());
+        
+        GridBagConstraints con = new GridBagConstraints();
+        
+
+        con.weightx = 0;
+        this.getContentPane().add(instructions, BorderLayout.NORTH);
+        
+        con.gridx = 0;
+        con.gridy = 1;
+        gridPanel.add(uNameLabel, con);
+        
+        con.gridx = 1;
+        con.gridy = 1;
+        gridPanel.add(username, con);
+        
+        
+        con.gridx = 0;
+        con.gridy = 2;
+        gridPanel.add(new JLabel(" "), con);
+        
+        con.gridx = 0;
+        con.gridy = 3;
+        gridPanel.add(pWordLabel, con);
+        
+        con.gridx = 1;
+        con.gridy = 3;
+        gridPanel.add(password, con);
+        
+        con.weightx = 1;
+        con.gridx = 3;
+        con.gridy = 4;
+        gridPanel.add(loginButton, con);
+        
+        this.getContentPane().add(gridPanel);
+        
+    }
 }
